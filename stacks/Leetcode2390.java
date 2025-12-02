ff12f9c2ded4ae72717d782d9a9a79a4fe821ee5
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-12-02 19:30:33 +0800

    leetcode 2390

---
+++ stacks/Leetcode2390.java
@@ -0,0 +1,71 @@
+package stacks;
+
+import java.util.Stack;
+
+
+// 2390. Removing Stars From a String
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a string s, which contains stars *.
+
+// In one operation, you can:
+
+// Choose a star in s.
+// Remove the closest non-star character to its left, as well as remove the star itself.
+// Return the string after all stars have been removed.
+
+// Note:
+
+// The input will be generated such that the operation is always possible.
+// It can be shown that the resulting string will always be unique.
+ 
+
+// Example 1:
+
+// Input: s = "leet**cod*e"
+// Output: "lecoe"
+// Explanation: Performing the removals from left to right:
+// - The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+// - The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+// - The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+// There are no more stars, so we return "lecoe".
+// Example 2:
+
+// Input: s = "erase*****"
+// Output: ""
+// Explanation: The entire string is removed, so we return an empty string.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of lowercase English letters and stars *.
+// The operation above can be performed on s.
+
+public class Leetcode2390 {
+    public String removeStars(String s) {
+    Stack<Character> stack = new Stack<>();
+    String ans = "";
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == "*".charAt(0)) {
+            stack.pop();
+        } else {
+            stack.push(s.charAt(i));
+        } 
+    }
+
+    for (int i = 0; i < stack.size(); i++) {
+        ans = ans + String.valueOf(stack.get(i));            
+    }
+    
+    return ans; 
+    }
+}
+
+// O(N) time complexity
+// O(N) space complexity
