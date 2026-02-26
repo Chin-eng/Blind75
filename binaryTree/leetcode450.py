b56e60b9ebad81a26f995d1ba239b0c138fcccb9
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-02-26 21:48:27 +0800

    Delete Node in a BST

---
+++ binaryTree/leetcode450.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left and not root.right:
+                return None
+            elif root.left:
+                return root.left
+            elif root.right:
+                return root.right
+            else:
+                node = self.rightSubTreeMin(root.right)
+                root.val = node.val
+                root.right = self.deleteNode(root.right, node.val)
+                return root
+
+        return root
+
+
+    def rightSubTreeMin(self, node: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not node.left and not node.right:
+            return node
+
+        minNode = self.rightSubTreeMin(node.left)
+
+        return minNode
